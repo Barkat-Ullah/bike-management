0f209663aed202139444928aa22f557d52003d58
Md Rakibul Hasan <hello.rakibh@gmail.com>
2025-04-16 15:30:35 +0600

    Book review starter

---
+++ src/app.ts
@@ -0,0 +1,49 @@
+import express, { Application, Request, Response } from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import router from './app/routes';
+import { globalErrorHandler } from './app/error/globalErrorHandler';
+import { notFoundRoute } from './app/error/notFoundRoute';
+
+// Create Express APP
+const app: Application = express();
+
+// List of allowed origins
+const allowedOrigins = ['http://localhost:3000'];
+
+const corsOptionsDelegate = function (req: Request, callback: any) {
+  const origin = req.header('Origin') as string;
+
+  let corsOptions;
+  if (allowedOrigins.indexOf(origin) !== -1) {
+    corsOptions = { origin: true, credentials: true };
+  } else {
+    corsOptions = { origin: false };
+  }
+  callback(null, corsOptions);
+};
+
+// Parser
+app.use(express.json());
+app.use(cookieParser());
+app.options('*', cors(corsOptionsDelegate));
+app.use(cors(corsOptionsDelegate));
+
+// Application Routes
+app.use('/api', router);
+
+// Create handler for GET request /
+const getRootController = (req: Request, res: Response) => {
+  res.send('Hello Express JS!');
+};
+
+// Route handler for /
+app.get('/', getRootController);
+
+// Global Error Handler
+app.use(globalErrorHandler);
+
+// Not Found Route
+app.use(notFoundRoute);
+
+export default app;
